0987b524c39d375c65762b1797be0be322cdff22
Alvin <alvinzcoxiv@gmail.com>
2020-08-05 18:13:49 -0400

    routes for html pages written and working

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const PORT = 8080
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(express.static('public'))
+
+const notes = []
+
+// Routes
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
+
+
+
+app.listen(PORT, function() {
+    console.log("Note Taker listening on PORT: " + PORT)
+})
